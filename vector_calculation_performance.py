187bfb47baf996c1da5425b8784786726eafc313
CaelumNuntium <st087042@student.spbu.ru>
2022-11-24 16:24:56 +0300

    test

--- vector_calculation_performance.py
+++ vector_calculation_performance.py
@@ -37,6 +37,7 @@ def run_test(repeat_times, a, b):
         a * b
         t2 = time.time()
         m_times.append(t2 - t1)
+        print(f"    #{k + 1}: {t2 - t1} s")
     return [average(m_times), sigma(m_times)]
 
 
@@ -48,9 +49,10 @@ def approx(f, x, y, n):
     return [[lb + j * h for j in range(n)], [f(lb + i * h, *(__ for __ in args)) for i in range(n)]]
 
 
+maxsize = 10000000
 repeat_times = 7
 num_measurements = 10
-max_lengths = [1000000, 1000, 100]
+max_lengths = [maxsize, math.ceil(math.sqrt(maxsize)), math.ceil(math.pow(maxsize, 1 / 3))]
 steps = [max_lengths[_] / num_measurements for _ in range(3)]
 c = ["#FF0000", "#00FF00", "#0000FF"]
 for i in range(3):
@@ -61,13 +63,16 @@ for i in range(3):
     times = []
     sigmas = []
     sizes = []
+    print(f"{i + 1}-dimension list:")
     for j in range(num_measurements):
-        sizes.append(((j + 1) * steps[i]) ** (i + 1))
+        sizes.append(math.ceil((j + 1) * steps[i]) ** (i + 1))
         a = make_list(i + 1, (j + 1) * steps[i])
         b = make_list(i + 1, (j + 1) * steps[i])
+        print(f"  list size {sizes[j]}")
         res = run_test(repeat_times, a, b)
         times.append(res[0])
         sigmas.append(res[1])
+        print(f"  average: {res[0]} s\n  error: {res[1]}\n")
     plt.errorbar(sizes, times, yerr=sigmas, fmt="o", ecolor=c[i], capsize=5, color=c[i], label=f"{i + 1}-dimension list")
     plt.legend(fontsize=10)
     curve = approx((lambda x, k, m: k * x + m), sizes, times, 1000)
@@ -80,15 +85,21 @@ for i in range(3):
     times = []
     sigmas = []
     sizes = []
+    print(f"{i + 1}-dimension array:")
     for j in range(num_measurements):
-        sizes.append(((j + 1) * steps[i]) ** (i + 1))
+        sizes.append(math.ceil((j + 1) * steps[i]) ** (i + 1))
         a = numpy.array(make_list(i + 1, (j + 1) * steps[i]))
         b = numpy.array(make_list(i + 1, (j + 1) * steps[i]))
+        print(f"  array size {sizes[j]}")
         res = run_test(repeat_times, a, b)
         times.append(res[0])
         sigmas.append(res[1])
+        print(f"  average: {res[0]} s\n  error: {res[1]}\n")
     plt.errorbar(sizes, times, yerr=sigmas, fmt="o", ecolor=c[i], capsize=5, color=c[i], label=f"{i + 1}-dimension array")
     plt.legend(fontsize=10)
     curve = approx((lambda x, k, m: k * x + m), sizes, times, 1000)
     plt.plot(curve[0], curve[1], color=c[i])
+fig = plt.gcf()
+fig.set_size_inches(12, 5.5)
+#fig.savefig("test.png", dpi=250)
 plt.show()
